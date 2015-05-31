8dece713a8f4a484bcaeb98e61fd098730180679
Dan Proudfoot <dan.proudfoot.iw@gmail.com>
2015-05-31 18:31:46 +0100

    Taking different approach to make the stuff work

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-var restify = require('express-restify-mongoose')
+var restify = require('express-restify-mongoose');
+var serveStatic = require('serve-static');
  
 mongoose.connect('mongodb://localhost/Doppler');
  
@@ -12,14 +13,33 @@ var songModel = require('./models/songs.js');
  
 
 var app = express();
+
+app.use(
+	function crossOrigin(req, res, next){
+		res.header("Access-Control-Allow-Origin", "*");
+		res.header("Access-Control-Allow-Headers", "X-Requested-With");
+		return next();
+});
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride());
- 
+app.use('/music/', serveStatic('/home/music/'));
+
 var router = express.Router();
+
+
+	
 restify.serve(router, albumModel);
-restify.serve(router, songModel);
+restify.serve(router, songModel, {
+	middleware:modify
+});
+
 app.use(router);
+
+function modify(req, res, next){
+	console.log(res.body);
+	next();
+}
 
 var port = 3610;
  
